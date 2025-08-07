ab75e785b6378c09c01d7e1ccf2887de3a6375c0
Renoaguilar <96883650+Renoaguilar@users.noreply.github.com>
2025-08-07 09:30:15 -0600

    Update segura_log_cleaner.sh

--- segura_log_cleaner.sh
+++ segura_log_cleaner.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+# Pass Senhasegura
+PASSWORD="Segura2025"
+
+read -s -p "🔐 Ingresa la contraseña para ejecutar este script: " user_pass
+echo ""
+if [[ "$user_pass" != "$PASSWORD" ]]; then
+  echo "❌ Contraseña incorrecta. Abortando..."
+  exit 1
+fi
+
 
 #                 _
 #                | |
