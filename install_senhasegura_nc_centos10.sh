db74effc543f1488e4c446fb769f457c431c2201
Renoaguilar <96883650+Renoaguilar@users.noreply.github.com>
2025-09-02 11:19:00 -0600

    Update install_senhasegura_nc_centos10.sh

--- install_senhasegura_nc_centos10.sh
+++ install_senhasegura_nc_centos10.sh
@@ -72,11 +72,14 @@ services:
     restart: unless-stopped
     networks:
       - senhasegura-network-connector
+    # publicar el puerto del agente para facilitar pruebas desde el host
+    ports:
+      - "${AGENT_PORT}:${AGENT_PORT}"
     environment:
-      SENHASEGURA_FINGERPRINT: ${SNC_FINGERPRINT}
-      SENHASEGURA_AGENT_PORT: ${AGENT_PORT}
-      SENHASEGURA_ADDRESSES: ${PAM_IPS}
-      SENHASEGURA_AGENT_SECONDARY: ${IS_SECONDARY}
+      SENHASEGURA_FINGERPRINT: "${SNC_FINGERPRINT}"
+      SENHASEGURA_AGENT_PORT: "${AGENT_PORT}"
+      SENHASEGURA_ADDRESSES: "${PAM_IPS}"
+      SENHASEGURA_AGENT_SECONDARY: "${IS_SECONDARY}"
 networks:
   senhasegura-network-connector:
     driver: bridge
